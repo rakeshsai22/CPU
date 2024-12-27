0660e127b2a1b336d1d0bd0b64a24c722324b0ad
snakkill <snakkill@en4226599rl.eng.asu.edu>
2024-12-26 21:23:05 -0700

    Version 2

--- singlecycle/rtl/adder.v
+++ singlecycle/rtl/adder.v
@@ -1,5 +1,12 @@
 module adder (
-    ports
+    input clk;
+    input res;
+    input reg[31:0]a;
+    input reg [31:0]b;
+    output sum;
 );
+    reg cout;
+    reg cin;
+    assign sum<= a+b;
     
 endmodule
